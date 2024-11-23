79335d77dd7f6140e43e5e1b3cd1f0c84277982b
Zukashikute <46180962+Zukashikute@users.noreply.github.com>
2024-11-23 11:51:07 +0800

    Small fix

--- cms/server.js
+++ cms/server.js
@@ -5,11 +5,15 @@ var http = require("http");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require('cors')
 
 // import the routing file to handle the default (index) route
 var index = require("./server/routes/app");
 
 // ... ADD CODE TO IMPORT YOUR ROUTING FILES HERE ...
+const messageRoutes = require("./server/routes/messages");
+const contactRoutes = require("./server/routes/contacts");
+const documentsRoutes = require("./server/routes/documents");
 
 var app = express(); // create an instance of express
 
@@ -46,10 +50,6 @@ app.use(express.static(path.join(__dirname, "dist/cms")));
 app.use("/", index);
 
 // ... ADD YOUR CODE TO MAP YOUR URL'S TO ROUTING FILES HERE ...
-const messageRoutes = require("./server/routes/messages");
-const contactRoutes = require("./server/routes/contacts");
-const documentsRoutes = require("./server/routes/documents");
-
 app.use("/messages", messageRoutes);
 app.use("/contacts", contactRoutes);
 app.use("/documents", documentsRoutes);
